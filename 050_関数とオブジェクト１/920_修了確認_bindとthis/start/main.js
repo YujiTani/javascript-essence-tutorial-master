ee705c3322cccb786c26af7a295d16f132ef2f51
yuzunosk55 <yuzuha3ds@gmail.com>
2020-12-11 23:54:39 +0900

    first commit

---
+++ 050_関数とオブジェクト１/920_修了確認_bindとthis/start/main.js
@@ -0,0 +1,61 @@
+
+const person = {
+    name: 'Tom',
+    bye() {
+        console.log('Bye ' + this.name);
+    },
+    hello: function (greeting) {
+        console.log(greeting + ' ' + this.name);
+        return greeting + ' ' + this.name;
+    },
+    hello1s: function () {
+        setTimeout(() => {
+            this.hello("hello");
+        }, 1000);
+    },
+    
+    /**
+     * 問題４：
+     * 1秒後に"hello Tom"
+     * と出力されるような、メソッドを
+     * personオブジェクトに追加してみてください。
+     * 
+     * 以下のように使用するものとします。
+     * `person.hello1s()` 
+     * -> 1秒後に"hello Tom"と出力
+     * 
+     * 3通りの方法で実装してみてください。
+     * １．bind
+     * ２．アロー関数
+     * ３．thisを一旦変数に代入
+     */
+
+
+}
+
+person.hello1s("hello");
+/**
+ * 問題１：
+ * 1秒後に"hello Tom"
+ * と出力されるように、以下のコード
+ * の記載を変更しましょう。
+ */
+//関数での実行になる
+// setTimeout(person.hello.bind(person , "hello") , 1000);
+
+/**
+ * 問題２：
+ * alertで"hello Tom"
+ * と出力されるように、
+ * 以下のコードを変更してください。
+ */
+// setTimeout(alert(person.hello("hello") , 1000));
+
+/**
+ * 問題３：
+ * person.byeメソッドを１秒後に実行したかったので
+ * bindを使って束縛してみましたが、コンソールには
+ * "Bye"しか表示されませんでした。
+ * "Bye Tom"とするためにはどうすればよいでしょうか？
+ */
+// setTimeout(person.bye.bind(person), 1000);
